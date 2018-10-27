3248f40cfe6e212fc59b87ffbede27b3f05e13c0
Reinhurd <roarus@yandex.ru>
2018-10-28 02:00:25 +0400

    Рефакторинг сценария создания нового пользователя, создан вспомогательный класс userinfo

--- test_Add_User.py
+++ test_Add_User.py
@@ -1,34 +1,51 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 import unittest
+from userinfo import UserInfo
 
 
 class TestAddUser(unittest.TestCase):
+
     def setUp(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
 
     def test_add_user(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-        wd.find_element_by_link_text("add new").click()
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_add_user_page(wd)
+        self.create_new_user(wd, UserInfo(firstname="fdsafsadf3", middlename="fdgdsgffd4", lastname="adgkasdfl5"))
+        self.logout(wd)
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def create_new_user(self, wd, UserInfo):
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("fgdsgsdgsdf")
+        wd.find_element_by_name("firstname").send_keys(UserInfo.firstname)
         wd.find_element_by_name("middlename").click()
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys("fdsgsdfgdg")
+        wd.find_element_by_name("middlename").send_keys(UserInfo.middlename)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("fgdsgdfsgsd")
+        wd.find_element_by_name("lastname").send_keys(UserInfo.lastname)
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-        wd.find_element_by_link_text("Logout").click()
+
+    def open_add_user_page(self, wd):
+        wd.find_element_by_link_text("add new").click()
+
+    def login(self, wd, username, password):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/")
 
     def tearDown(self):
         self.wd.quit()
---
+++ userinfo.py
@@ -0,0 +1,8 @@
+class UserInfo:
+    """
+    Fullfill user info on create-new-users page.
+    """
+    def __init__(self, firstname, middlename, lastname):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
