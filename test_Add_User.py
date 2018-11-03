19996f8f67943465e367b5d4bb5e156dbdf17a9f
Reinhurd <roarus@yandex.ru>
2018-11-04 02:06:03 +0400

    Рефакторинг тестов добавления групп и пользователей, Добавлено: замена unittest на pytest, использование фикстур. Теперь тесты - это обычные функции

--- test_Add_User.py
+++ test_Add_User.py
@@ -1,55 +1,20 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-import unittest
+import pytest
+from application import Application
 from userinfo import UserInfo
 
 
-class TestAddUser(unittest.TestCase):
-
-    def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
-
-    def test_add_user(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.open_add_user_page(wd)
-        self.create_new_user(wd, UserInfo(firstname="fdsafsadf3", middlename="fdgdsgffd4", lastname="adgkasdfl5"))
-        self.logout(wd)
-
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
-
-    def create_new_user(self, wd, userinfo):
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(userinfo.firstname)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(userinfo.middlename)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(userinfo.lastname)
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-
-    def open_add_user_page(self, wd):
-        wd.find_element_by_link_text("add new").click()
-
-    def login(self, wd, username, password):
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-
-    def open_home_page(self, wd):
-        wd.get("http://localhost/addressbook/")
-
-    def tearDown(self):
-        self.wd.quit()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_add_user(app):
+    app.login(username="admin", password="secret")
+    app.create_new_user(UserInfo(firstname="fdsafsadf3", middlename="fdgdsgffd4", lastname="adgkasdfl5"))
+    app.logout()
+
+
+
