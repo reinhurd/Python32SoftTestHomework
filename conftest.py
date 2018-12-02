d3ed0a60a31c6a638f719beb6f7bd7a02f9bb1a5
reinhurd <roarus@yandex.ru>
2018-12-02 16:13:35 +0400

    Добавлена поддержка командной строки - выбор браузера (IE, Chrome,Firefox) и стартовой страницы проекта

--- conftest.py
+++ conftest.py
@@ -8,11 +8,13 @@ fixture = None
 @pytest.fixture
 def app(request):
     global fixture
+    browser = request.config.getoption("--browser")
+    baseurl = request.config.getoption("--baseurl")
     if fixture is None:
-        fixture = Application()
+        fixture = Application(browser=browser, baseurl=baseurl)
     else:
         if not fixture.is_valid():
-            fixture = Application()
+            fixture = Application(browser=browser, baseurl=baseurl)
     fixture.session.ensure_login(username="admin", password="secret")
     return fixture
 
@@ -26,3 +28,7 @@ def stop(request):
     request.addfinalizer(fin)
     return fixture
 
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="Firefox")
+    parser.addoption("--baseurl", action="store", default="http://localhost/addressbook/")
--- fixture/application.py
+++ fixture/application.py
@@ -6,11 +6,19 @@ from fixture.user import UserHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, baseurl):
+        if browser == "Firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "Chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "Ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.user = UserHelper(self)
+        self.baseurl = baseurl
         self.open_home_page()
 
     def is_valid(self):
@@ -23,7 +31,7 @@ class Application:
     def open_home_page(self):
         wd = self.wd
         if not wd.current_url.endswith("/addressbook/"):
-            wd.get("http://localhost/addressbook/")
+            wd.get(self.baseurl)
 
     def destroy(self):
         self.wd.quit()
