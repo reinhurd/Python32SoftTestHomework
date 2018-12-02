8d377c2ae6dc28682ffe20fc78b6ef3fd732cd6a
reinhurd <roarus@yandex.ru>
2018-12-02 17:53:34 +0400

    Реализация загрузки информации о приложении из файла

--- conftest.py
+++ conftest.py
@@ -1,21 +1,24 @@
 import pytest
 from fixture.application import Application
-
+import json
+import os.path
 
 fixture = None
+target = None
 
 
 @pytest.fixture
 def app(request):
     global fixture
+    global target
     browser = request.config.getoption("--browser")
-    baseurl = request.config.getoption("--baseurl")
-    if fixture is None:
-        fixture = Application(browser=browser, baseurl=baseurl)
-    else:
-        if not fixture.is_valid():
-            fixture = Application(browser=browser, baseurl=baseurl)
-    fixture.session.ensure_login(username="admin", password="secret")
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:
+            target = json.load(f)
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, baseurl=target['baseurl'])
+    fixture.session.ensure_login(username=target['username'], password=target['password'])
     return fixture
 
 
@@ -31,4 +34,5 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="Firefox")
-    parser.addoption("--baseurl", action="store", default="http://localhost/addressbook/")
+    parser.addoption("--target", action="store", default="target.json")
+
