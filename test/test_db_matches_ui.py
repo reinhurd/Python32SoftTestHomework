aaadb5e0875f86d96cb59a515c832c9ea316bf74
Reinhurd <roarus@yandex.ru>
2018-12-09 22:29:06 +0400

    Создан тест сравнения контактов в БД и интерфейсе

--- test/test_db_matches_ui.py
+++ test/test_db_matches_ui.py
@@ -1,5 +1,5 @@
 from model.group import Group
-from timeit import timeit
+from model.userinfo import UserInfo
 
 
 def test_group_list_compare_ui_and_db(app, db):
@@ -8,3 +8,11 @@ def test_group_list_compare_ui_and_db(app, db):
     ui_group = app.group.get_group_list()
     db_groups = db.get_group_list()
     assert sorted(ui_group, key=Group.id_or_max) == sorted(db_groups, key=Group.id_or_max)
+
+
+def test_contacts_list_compare_ui_and_db(app, db):
+    if len(db.get_userinfo_list()) == 0:
+        app.user.create(UserInfo(firstname="Test", lastname="TEST"))
+    db_users = db.get_userinfo_list()
+    ui_users = app.user.get_users_list()
+    assert sorted(ui_users, key=UserInfo.id_or_max) == sorted(db_users, key=Group.id_or_max)
