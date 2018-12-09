50f4ff5f4ed7078aee2b69eef90ffe6821eda22f
Reinhurd <roarus@yandex.ru>
2018-12-09 22:18:40 +0400

    Создан тест сравнения всех групп в интерфейсе и Базе Данных

--- test/test_db_matches_ui.py
+++ test/test_db_matches_ui.py
@@ -2,10 +2,9 @@ from model.group import Group
 from timeit import timeit
 
 
-def test_group_list(app, db):
-    pass
-    # print(timeit(lambda: app.group.get_group_list(), number=1))
-    # def clean(group):
-    #     return Group(id=group.id, name=group.name.strip())
-    # print(timeit(lambda: map(clean, db.get_group_list()), number=1000))
-    # assert False #sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
+def test_group_list_compare_ui_and_db(app, db):
+    if len(db.get_group_list()) == 0:
+        app.group.create(Group(name="Test", header="Test", footer="Test"))
+    ui_group = app.group.get_group_list()
+    db_groups = db.get_group_list()
+    assert sorted(ui_group, key=Group.id_or_max) == sorted(db_groups, key=Group.id_or_max)
