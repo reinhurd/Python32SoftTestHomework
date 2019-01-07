716c0bb7595ba525d0bb1377bdcbef1234a55cf9
Reinhurd <roarus@yandex.ru>
2019-01-08 02:12:32 +0400

    Создан отчет Allure по всем тестам. Для теста создания групп описаны шаги с помощью метода allure.step

--- test/test_Add_Group.py
+++ test/test_Add_Group.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
-
+import pytest
+import allure
 
 def test_add_group(app, db, json_groups, check_ui):
     group = json_groups
-    old_groups = db.get_group_list()
-    app.group.create(group)
-    new_groups = db.get_group_list()
-    old_groups.append(group)
-    assert old_groups == new_groups
-    if check_ui:
-        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    with allure.step('Given a group list'):
+        old_groups = db.get_group_list()
+    with allure.step('When I add the group %s to the list' % group):
+        app.group.create(group)
+    with allure.step('Then the new group list is equal to the old list with the added group'):
+        new_groups = db.get_group_list()
+        old_groups.append(group)
+        assert old_groups == new_groups
+        if check_ui:
+            assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
